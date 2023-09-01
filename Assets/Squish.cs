c7dbce72e66668da1f5362de8bf02d23ac8fdb68
hmay01 <harryjmay1@gmail.com>
2023-09-01 16:44:36 +0100

    remove pitch on bounce

--- Assets/Squish.cs
+++ Assets/Squish.cs
@@ -10,11 +10,7 @@ public class Squish : MonoBehaviour
     float maxCollisionMagnitude;
     int floorLayer;
 
-    AudioSource audioSource;
 
-    public float pitchLow;
-
-    float pitch;
 
     //new
     public float squishTime;
@@ -27,14 +23,11 @@ public class Squish : MonoBehaviour
         initialScale = transform.localScale;
         floorLayer = 6;
         maxCollisionMagnitude = 17;
-        audioSource = GetComponent<AudioSource>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        audioSource.pitch = pitch;
     }
 
     void OnCollisionEnter(Collision collision)
@@ -71,7 +64,6 @@ public class Squish : MonoBehaviour
             while (elapsedTime < squishTime)
             {
                 transform.localScale = Vector3.Lerp(initialScale, squishScale, (elapsedTime / squishTime));
-                pitch = Mathf.Lerp (pitchLow, 1, Mathf.InverseLerp (squishX, initialScale.x, transform.localScale.x));
                 elapsedTime += Time.deltaTime;
                 yield return new WaitForEndOfFrame();
             }
